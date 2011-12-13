a236bf313ad7e2d56d2c3b01006849005ab62529
Daniel Levin <dlevin@net.t-labs.tu-berlin.de>
2011-12-13 19:38:27 +0100

    Factored out all workload code to workload.py module

--- ctrlsim.py
+++ ctrlsim.py
@@ -3,11 +3,12 @@
 # Dan Levin <dlevin@net.t-labs.tu-berlin.de>
 # Brandon Heller <brandonh@stanford.edu>
 
-from math import sqrt, floor
+from math import sqrt
 import networkx as nx
-import sys
 from random import choice, randint, random
+import sys
 import unittest
+from workload import *
 
 
 class Controller(object):
@@ -191,10 +192,12 @@ class LinkBalancerSim(Simulation):
         return sqrt(sum(values))
 
     def rmse_servers(self, graph=None):
-        """Calculate RMSE over server outgoing links"""
-        # Compute ideal used fraction of each server's outgoing link, assuming
-        # perfect split between them (regardless of discrete demands with
-        # bin-packing constraints).
+        """
+        Calculate RMSE over server outgoing links:
+        Compute ideal used fraction of each server's outgoing link, assuming
+        perfect split between them (regardless of discrete demands with
+        bin-packing constraints).
+        """
         if not graph:
             graph = self.graph
 
@@ -227,7 +230,7 @@ class LinkBalancerSim(Simulation):
 
     def allocate_resources(self, path=[], resources=0, whenfree=0):
         """
-        Subtract resources along path for each link (add to self.used[src][dst])
+        Subtract resources for each link in path (add to self.used[src][dst])
         Detect if any link in a path is fully utilized, do not oversubscribe
         Record the resources for link to be freed at time <whenfree>
         """
@@ -315,7 +318,7 @@ class LinkBalancerSim(Simulation):
 ###############################################################################
 
 
-class SimulatorTest(unittest.TestCase):
+class TestSimulator(unittest.TestCase):
     """Unit tests for LinkBalancerSim class"""
     graph = nx.DiGraph()
     graph.add_nodes_from(['sw1', 'sw2'], type='switch')
@@ -410,138 +413,6 @@ class SimulatorTest(unittest.TestCase):
         self.assertEqual(len(sim.active_flows.keys()), 0)
 
 ###############################################################################
-
-
-def unit_workload(switches, size, duration, timesteps):
-    """
-    Return workload description with unit demands and unit length.
-
-    switches: list of switch names
-    size: data demand (unitless)
-    duration: length of each request (unitless)
-    timesteps: number of timesteps
-    returns: workload structure
-        # Workload is a list of lists.
-        # Each top-level list element corresponds to one time step.
-        # Each second-level list element is a tuple of:
-        #   (switch, size, duration)
-    """
-    workload = []
-    for t in range(timesteps):
-        requests = [(sw, size, duration) for sw in switches]
-        workload.append(requests)
-    return workload
-
-
-def random_workload(switches, size, duration, timesteps):
-    """
-    Return workload description with unit demands and unit length.
-    """
-    workload = []
-    minutil = 10
-    maxutil = 10
-    mindur = 1
-    maxdur = 1
-    for t in range(timesteps):
-        requests = [(choice(sw), randint(minutil, maxutil),
-                     randint(mindur, maxdur)) for sw in switches]
-        workload.append(requests)
-    return workload
-
-
-def generic_workload(switch_workload_fcns, size, duration, timesteps):
-    """
-    Return workload description based on input functions for each switch
-
-    NOTE: when duration != timestep, requests will overlap, such that
-    the actual desired BW will not match the total request bandwidth.
-    NOTE: requests are equal in size
-    TODO: Generalize traffic generation to better model changing demand and
-    distributions.
-
-    switches_workload_fcns: dict of switch names to workload functions
-        A workload function returns the total demand at a given time.
-        Its only input is the current timestep.
-    size: bw of each request (unitless)
-        Requests are CBR and bin-packed until no more space remains.
-        TODO: Generalize the size/duration fields to support a type of UDP or
-            TCP.  Size/duration would only be needed for UDP then.
-    duration: length of each request (unitless)
-        Requests are CBR.
-    timesteps: number of timesteps
-    returns: workload structure
-        # Workload is a list of lists.
-        # Each top-level list element corresponds to one time step.
-        # Each second-level list element is a tuple of:
-        #   (switch, size, duration)
-    """
-    workload = []
-    switches = sorted(switch_workload_fcns.keys())
-    for t in range(timesteps):
-        requests = []
-        for sw in switches:
-            total_demand = switch_workload_fcns[sw](t)
-            # Approximate desired demand based on size
-            num_requests = int(floor(total_demand / float(size)))
-            for req in range(num_requests):
-                requests.append((sw, size, duration))
-        workload.append(requests)
-    return workload
-
-
-def sawtooth(t, period, offset, max_demand):
-    """Sawtooth: 0 to full to 0 with specified period"""
-    phase = (t + offset) % float(period)
-    if phase < period / 2.0:
-        return phase / float(period / 2.0) * max_demand
-    else:
-        return (period - phase) / float(period / 2.0) * max_demand
-
-
-def dual_sawtooth_workload(switches, period, offset, max_demand, size,
-                           duration, timesteps):
-    """
-    Return workload description with offset sawtooths.
-
-    switches: two-element list with switch names
-    period: sawtooth period (unitless)
-    offset: sawtooth shift, same time units as period
-    max_demand: maximum demand to start up during a timestep (unitless)
-    size: data demand (unitless)
-    duration: length of each request (unitless)
-    timesteps: number of timesteps
-    returns: workload structure
-        # Workload is a list of lists.
-        # Each top-level list element corresponds to one time step.
-        # Each second-level list element is a tuple of:
-        #   (switch, size, duration)
-    """
-    assert len(switches) == 2
-    switch_workload_fcns = {
-        switches[0]: lambda t: sawtooth(t, period, 0, max_demand),
-        switches[1]: lambda t: sawtooth(t, period, offset, max_demand)
-    }
-    return generic_workload(switch_workload_fcns, size, duration, timesteps)
-
-
-class TestSawtoothWorkload(unittest.TestCase):
-
-    def test_sawtooth(self):
-        """Verify sawtooth function value extremes."""
-        for period in [4, 5, 8, 10]:
-            max_demand = 10
-            reps = 2  # Repetition of full waveforms
-            st_fcn = lambda t: sawtooth(t, period = period, offset = 0,
-                                        max_demand = max_demand)
-            st_offset_fcn = lambda t: sawtooth(t, period = period,
-                                               offset = period / 2.0,
-                                               max_demand = max_demand)
-            for i in range(reps):
-                self.assertEquals(st_fcn(i * period), 0)
-                self.assertEquals(st_offset_fcn(i * period), max_demand)
-                self.assertEquals(st_fcn(i * period + period / 2.0), max_demand)
-                self.assertEquals(st_offset_fcn(i * period + period / 2.0), 0)
-
 
 def two_switch_topo():
     graph = nx.DiGraph()
