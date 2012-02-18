720e8a9ed45a430f1e1cebb0c745d46b097c0fd0
Daniel Levin <dlevin@net.t-labs.tu-berlin.de>
2012-02-18 18:48:45 +0100

    - Added a boxplot to compare timeseries-rmse values across individual runs - Added 4 proposed evaluation scenarios for our submission, and one trivial demo evaluation scenario to runsim.py

--- plot/plot.py
+++ plot/plot.py
@@ -13,33 +13,35 @@ import json
 import matplotlib.pyplot as plt
 import plot_helper as ph
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--files', '-f',
+                    help="input files",
+                    action="store",
+                    nargs='+',
+                    dest="files")
+parser.add_argument('--dummy-data', '-d',
+                    help="Output a dummy datafile as example for import.",
+                    default=False,
+                    action='store_true',
+                    dest="dummydata")
+parser.add_argument('--out', '-o',
+                    help="Output pdf file for the plot.",
+                    default=None,
+                    dest="out")
+
+args = parser.parse_args()
+
+
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--files', '-f',
-                        help="input files",
-                        action="store",
-                        nargs='+',
-                        dest="files")
-    parser.add_argument('--dummy-data', '-d',
-                        help="Output a dummy datafile as example for import.",
-                        default=False,
-                        action='store_true',
-                        dest="dummydata")
-    parser.add_argument('--out', '-o',
-                        help="Output pdf file for the plot.",
-                        default=None,
-                        dest="out")
-    args = parser.parse_args()
-
-
     if args.dummydata:
         ph.write_dummy_data()
     elif args.files:
         plot_timeseries()
+        plot_boxplot()
     else:
         parser.print_help()
 
-def plot_timeseries():
+def plot_timeseries(saveplot=False):
     cgen = ph.colorGenerator()
     fgen = ph.fmtGenerator()
     for f in args.files:
@@ -74,8 +76,48 @@ def plot_timeseries():
     plt.grid()
     plt.legend()
 
-    if args.out:
-        plt.savefig(args.out)
+    if saveplot:
+        plt.savefig(saveplot)
+    else:
+        plt.show()
+
+def plot_boxplot(saveplot=False):
+    cgen = ph.colorGenerator()
+    fgen = ph.fmtGenerator()
+    data = []
+    for f in args.files:
+        ff = open(f, 'r')
+        j = json.loads("".join(ff.readlines()).strip())
+        data.append(j["rmse_servers"])
+        trace = j["simulation_trace"]
+        ingress = [ i['ingress'] for i in trace ]
+        ingress_switches = {}
+        ingress_switch_vals = {}
+        # collect all switches which show ingress at some point
+        for i in ingress:
+            for k,v in i.iteritems():
+               ingress_switches.setdefault(k,[])
+
+        for i in ingress:
+            for switch in ingress_switches.keys():
+                if switch in i.keys():
+                    value = i[switch]
+                else:
+                    value = 0
+                ingress_switch_vals.setdefault(switch,[]).append(value)
+
+        ff.close()
+
+    plt.boxplot(data)
+    plt.title("Boxplot" + str(f))
+    plt.ylabel("")
+    #TODO ennumerate boxplots
+    plt.xlabel("sync_period")
+    plt.grid()
+    plt.legend()
+
+    if saveplot:
+        plt.savefig(saveplot)
     else:
         plt.show()
 
--- runsim.py
+++ runsim.py
@@ -5,37 +5,104 @@
 import argparse
 import logging
 import logging.config
+#TODO import plot #automatically plot selected ouputs directly after running
 #import sim.controller as c
 from sim.simulation import LinkBalancerSim
 from sim.workload import dual_offset_workload, wave
 import sys
-from test.test_helper import two_ctrls, two_switch_topo
+from test.test_helper import two_ctrls, two_switch_topo, strictly_local_ctrls
 
 logging.config.fileConfig('setup.cfg')
 logger = logging.getLogger(__name__)
 
-parser = argparse.ArgumentParser()
-args = parser.parse_args()
-
 def main():
-    logger.info("Starting Runsim")
-    two_ctrl_wave_inphase()
-    logger.info("Ending Runsim")
+    demo_strictly_local_ctrls()
+    sync_improves_metric()
+    synced_dist_equals_central()
+    #TODO enable showing plots directly after runs in addition to logging them
 
-def two_ctrl_wave_inphase(max_demand=8, show_graph=False):
-    """For in-phase sawtooth with 2 synced ctrls, ensure server RMSE == 0."""
+def demo_strictly_local_ctrls(max_demand=8, show_graph=False):
+    """Demonstrate synchronization across domains makes no difference when
+    LinkBalanerCtrl only handles requests within its own domain"""
+
+    #TODO: demonstrate with more than 1 srv per controller domain
+    period = 16
+    timesteps = period * 2
+    for sync_period in range(0, timesteps):
+        myname = sys._getframe().f_code.co_name
+        logger.info("starting %s", myname)
+        workload = dual_offset_workload(switches=['sw1', 'sw2'],
+                                        period=period, offset=period / 2.0,
+                                        max_demand=max_demand, size=1,
+                                        duration=1, timesteps=timesteps,
+                                        workload_fcn=wave)
+
+        ctrls = strictly_local_ctrls()
+        sim = LinkBalancerSim(two_switch_topo(), ctrls)
+        myname = sys._getframe().f_code.co_name + "_" + str(sync_period)
+        sim.run_and_trace(myname, workload, old=True, sync_period=sync_period,
+                          show_graph=show_graph)
+        logger.info("ending %s", myname)
+
+def sync_improves_metric(max_demand=8, show_graph=False):
+    """Evalute the value of synchronization for a LinkBalanerCtrl by showing
+    its effect on performance metric. We expect that for a workload which
+    imparts server link imbalance across multiple domains, syncing will help
+    improve the rmse_server metric."""
+
+    period = 16
+    timesteps = period * 2
+    for sync_period in range(0, timesteps):
+        myname = sys._getframe().f_code.co_name
+        logger.info("starting %s", myname)
+        workload = dual_offset_workload(switches=['sw1', 'sw2'],
+                                        period=period, offset=period / 2.0,
+                                        max_demand=max_demand, size=1,
+                                        duration=1, timesteps=timesteps,
+                                        workload_fcn=wave)
+
+        ctrls = two_ctrls()
+        sim = LinkBalancerSim(two_switch_topo(), ctrls)
+        myname = sys._getframe().f_code.co_name + "_" + str(sync_period)
+        sim.run_and_trace(myname, workload, old=True, sync_period=sync_period,
+                          show_graph=show_graph)
+        logger.info("ending %s", myname)
+
+
+def synced_dist_equals_central(max_demand=8, show_graph=False):
+    """Ensure that a distributed controller simulation run with sync_period=0
+    yields exactly the same result as the same toplology and workload with a
+    single controller."""
+
     period = 8 
     timesteps = period * 2
-    workload = dual_offset_workload(switches=['sw1', 'sw2'],
-                                    period=period, offset=0,
-                                    max_demand=max_demand, size=1,
-                                    duration=1, timesteps=timesteps,
-                                    workload_fcn=wave)
+    for sync_period in range(0, timesteps):
+        myname = sys._getframe().f_code.co_name
+        logger.info("starting %s", myname)
+        workload = dual_offset_workload(switches=['sw1', 'sw2'],
+                                        period=period, offset=0,
+                                        max_demand=max_demand, size=1,
+                                        duration=1, timesteps=timesteps,
+                                        workload_fcn=wave)
 
-    ctrls = two_ctrls()
-    sim = LinkBalancerSim(two_switch_topo(), ctrls)
-    myname = sys._getframe().f_code.co_name
-    sim.run_and_trace(myname, workload, old=True, sync_period=timesteps,
-                      show_graph=show_graph)
+        ctrls = two_ctrls()
+        sim = LinkBalancerSim(two_switch_topo(), ctrls)
+        myname = sys._getframe().f_code.co_name
+        sim.run_and_trace(myname+str(sync_period), workload, old=True, sync_period=sync_period,
+                          show_graph=show_graph)
+        logger.info("ending %s", myname)
 
-main()
+def compare_greedy_dist_to_centralized(max_demand=8, show_graph=False):
+    """Ensure that a distributed controller simulation run with sync_period=0
+    yields exactly the same result as the same toplology and workload with a
+    single controller."""
+    pass
+
+def compare_greedy_dist_to_sync_dist(max_demand=8, show_graph=False):
+    """Understand what improvement synchronization gives us over a greedy
+    algorithm in a dynamic, discrete loadbalancing environment"""
+    pass
+
+
+if __name__ == "__main__":
+    main()
