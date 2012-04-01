d154e2984a7a9389a40fb0dc72d28a5a0fbf29c7
Daniel Levin <dlevin@net.t-labs.tu-berlin.de>
2012-04-01 23:38:34 +0200

    Fixed a bug in the to_txt.sh script which wasn't converting the right json files to column-formatted text

--- plot/to_txt.sh
+++ plot/to_txt.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 # RMSE timeseries
-for i in sync_improves_metric_lbc_expo sync_improves_metric_lbc_wave sync_improves_metric_separate_expo sync_improves_metric_separate_wave; do echo $i; ../plot/json2txt.py -f ${i}_32_{00,01,02,04,08,16}_0.metrics -r > R_rmse_${i}_32_0.metrics; done;
+for i in sync_improves_metric_lbc_expo sync_improves_metric_lbc_wave sync_improves_metric_separate_expo sync_improves_metric_separate_wave; do 
+	echo "../plot/json2txt.py -f ${i}_32_{00,01,02,04,08,16}_0.metrics -r > R_rmse_${i}_32_0.metrics"
+	../plot/json2txt.py -f ${i}_32_{00,01,02,04,08,16}_0.metrics -r > R_rmse_${i}_32_0.metrics; 
+done;
 
 #Physical network View files
-for i in sync_improves_metric*.metrics; do echo $i; ../plot/json2txt.py -p -f $i > R_pnview_$i; done;
-
-
+for i in sync_improves_metric*.metrics; do echo "$i; ../plot/json2txt.py -p -f $i to R_pnview_$i"; ../plot/json2txt.py -p -f $i > R_pnview_$i; done;
