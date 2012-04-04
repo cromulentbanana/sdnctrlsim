8a5de80450c340315f50c96a735d571be22d3f50
Daniel Levin <dlevin@net.t-labs.tu-berlin.de>
2012-04-04 21:59:42 +0200

    Took a hard-coded index value from Anja's R script and made it into a function parameter

--- plot/ctrlsim.R
+++ plot/ctrlsim.R
@@ -4,6 +4,8 @@ plot.single.file <- function(
 	#dir = "//mnt/raid5_data/stanford-ofnet/virtpyenv/logs/",
 	#file="anja_rmse_sync_improves_metric_lbc_wave_32_0.txt",
 	file,
+	slice.start = 112,
+	slice.length = 35,
 	do.ps=0, psdir="/tmp/", psbase="", m.max=1000, n=4, do.boxplot=0, do.ts=1)
 {
 
@@ -19,18 +21,19 @@ plot.single.file <- function(
 #    xrange <- c(1,length(data[,1]))
 #    yrange <- range(data)
 # TODO: Make the start:end values parameters
-    xrange <- c(1,length(data[,1][112:147]))
+    xrange <- c(1,length(data[,1][slice.start:(slice.start+slice.length)]))
+    #xrange <- c(slice.start, slice.start+slice.length)
     yrange <- range(data)
     plot(xrange, yrange, xlab = "Simulation Time", 
       	ylab = "% Link Utilization", type = "n",
       	cex = 2, cex.axis = 2, cex.lab = 2)
-    leg = paste("Link ", 1:n)
+	leg = paste("Link ", c("sw1-sw2", "srv1-sw1", "srv2-sw2", "sw2-sw1"))
     do.col <- c(1:n, 1:n)
     #legend(x=1,y=0.33, xrange[1], yrange[2], col = do.col[1:n], pch=c(1:n), legend = leg, bty = "n", lty = rep(1, n), cex = 1.5)
     legend(x=2,y=0.33, col = do.col[1:n], pch=c(1:n), legend = leg, bty = "n", lty = rep(1, n), cex = 1.5)
     for(i in 1:n) {
-      points(data[, i][112:147], col = do.col[i], pch = i, cex = 2)
-      lines(data[, i][112:147], col = do.col[i], pch = i, cex = 2)
+      points(data[, i][slice.start:(slice.start+slice.length)], col = do.col[i], pch = i, cex = 2)
+      lines(data[, i][slice.start:(slice.start+slice.length)], col = do.col[i], pch = i, cex = 2)
     }
     abline(v=c(0:16)*16 + 2)
     if(do.ps) {
